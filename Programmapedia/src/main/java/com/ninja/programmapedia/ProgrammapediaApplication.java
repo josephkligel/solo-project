048bb63dd4952b491412ed16392631c26ffbfc09
Joseph Kligel <jkligel@gmail.com>
2023-07-04 10:23:14 -0500

    Finally

--- Programmapedia/src/main/java/com/ninja/programmapedia/ProgrammapediaApplication.java
+++ Programmapedia/src/main/java/com/ninja/programmapedia/ProgrammapediaApplication.java
@@ -1,13 +1,28 @@
 package com.ninja.programmapedia;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class ProgrammapediaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ProgrammapediaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammapediaApplication.class, args);
+    }
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        Connector ajpConnector = new Connector("AJP/1.3");
+        ajpConnector.setPort(9090);
+        ajpConnector.setSecure(false);
+        ajpConnector.setAllowTrace(false);
+        ajpConnector.setScheme("http");
+((AbstractAjpProtocol)ajpConnector.getProtocolHandler()).setSecretRequired(false);
+        tomcat.addAdditionalTomcatConnectors(ajpConnector);
+        return tomcat;
+    }
 }
+
+
